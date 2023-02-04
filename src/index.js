49de4a36308bfe676d0c1136a107240ddc6df33e
Gilad Levi <gilad.x@gmail.com>
2023-02-04 12:19:48 +0200

    react push

--- src/index.js
+++ src/index.js
@@ -2,15 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 
-import './style.css'
-import Home from './views/home'
 
 const App = () => {
   return (
     <Router>
-      <div>
         <Route component={Home} exact path="/" />
-      </div>
     </Router>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -57,6 +57,5 @@ module.exports = {
   },
   "plugins": [],
   "content": [
-    "./src/**/*.{js,ts,jsx,tsx}"
   ]
 }
