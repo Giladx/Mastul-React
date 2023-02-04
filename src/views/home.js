e862e876d78bf72e6dc8abda14ebbbf1fc7e3597
Gilad Levi <gilad.x@gmail.com>
2023-02-04 12:07:27 +0200

    Commit from TeleportHQ

---
+++ src/views/home.js
@@ -0,0 +1,287 @@
+import React from 'react'
+
+import { Helmet } from 'react-helmet'
+
+import FeatureCard4 from '../components/feature-card4'
+import GalleryCard2 from '../components/gallery-card2'
+import BlogPostCard4 from '../components/blog-post-card4'
+import TestimonialCard2 from '../components/testimonial-card2'
+import TestimonialCard4 from '../components/testimonial-card4'
+import './home.css'
+
+const Home = (props) => {
+  return (
+    <div className="home-container">
+      <Helmet>
+        <title>Mas`tul</title>
+        <meta name="description" content=" Club " />
+        <meta property="og:title" content="Mas`tul" />
+        <meta property="og:description" content=" Club " />
+        <meta
+          property="og:image"
+          content="https://aheioqhobo.cloudimg.io/v7/_playground-bucket-v2.teleporthq.io_/af469c8d-b822-45c5-80f3-94b54b452234/05a95b07-91ac-4e1d-aca9-4de211ba2a09?org_if_sml=1"
+        />
+      </Helmet>
+      <header data-thq="thq-navbar" className="home-navbar-interactive">
+        <img
+          alt="logo"
+          src="/playground_assets/mastulogo-1500h.webp"
+          className="home-logo"
+        />
+        <div
+          data-thq="thq-navbar-nav"
+          data-role="Nav"
+          className="home-desktop-menu"
+        >
+          <nav
+            data-thq="thq-navbar-nav-links"
+            data-role="Nav"
+            className="home-nav"
+          >
+            <span className="home-text">About</span>
+            <span className="home-text01">Resort</span>
+            <span className="home-text02">Restaurant</span>
+            <span className="home-text03">Club</span>
+            <span className="home-text04">Stuff</span>
+          </nav>
+        </div>
+        <div data-thq="thq-navbar-btn-group" className="home-btn-group">
+          <button className="home-login button">Contact</button>
+          <button className="home-register button">Order</button>
+        </div>
+        <div data-thq="thq-burger-menu" className="home-burger-menu">
+          <svg viewBox="0 0 1024 1024" className="home-icon">
+            <path d="M128 554.667h768c23.552 0 42.667-19.115 42.667-42.667s-19.115-42.667-42.667-42.667h-768c-23.552 0-42.667 19.115-42.667 42.667s19.115 42.667 42.667 42.667zM128 298.667h768c23.552 0 42.667-19.115 42.667-42.667s-19.115-42.667-42.667-42.667h-768c-23.552 0-42.667 19.115-42.667 42.667s19.115 42.667 42.667 42.667zM128 810.667h768c23.552 0 42.667-19.115 42.667-42.667s-19.115-42.667-42.667-42.667h-768c-23.552 0-42.667 19.115-42.667 42.667s19.115 42.667 42.667 42.667z"></path>
+          </svg>
+        </div>
+        <div data-thq="thq-mobile-menu" className="home-mobile-menu">
+          <div
+            data-thq="thq-mobile-menu-nav"
+            data-role="Nav"
+            className="home-nav1"
+          >
+            <div className="home-container01">
+              <img
+                alt="image"
+                src="/playground_assets/mastulogo-1500h.webp"
+                className="home-image"
+              />
+              <div data-thq="thq-close-menu" className="home-menu-close">
+                <svg viewBox="0 0 1024 1024" className="home-icon02">
+                  <path d="M810 274l-238 238 238 238-60 60-238-238-238 238-60-60 238-238-238-238 60-60 238 238 238-238z"></path>
+                </svg>
+              </div>
+            </div>
+            <nav
+              data-thq="thq-mobile-menu-nav-links"
+              data-role="Nav"
+              className="home-nav2"
+            >
+              <span className="home-text05">About</span>
+              <span className="home-text06">Resort</span>
+              <span className="home-text07">Restaurant</span>
+              <span className="home-text08">Club</span>
+              <span className="home-text09">Stuff</span>
+            </nav>
+            <div className="home-container02">
+              <button className="home-login1 button">Login</button>
+              <button className="button home-register1">Register</button>
+            </div>
+          </div>
+          <div className="home-icon-group">
+            <svg viewBox="0 0 950.8571428571428 1024" className="home-icon04">
+              <path d="M925.714 233.143c-25.143 36.571-56.571 69.143-92.571 95.429 0.571 8 0.571 16 0.571 24 0 244-185.714 525.143-525.143 525.143-104.571 0-201.714-30.286-283.429-82.857 14.857 1.714 29.143 2.286 44.571 2.286 86.286 0 165.714-29.143 229.143-78.857-81.143-1.714-149.143-54.857-172.571-128 11.429 1.714 22.857 2.857 34.857 2.857 16.571 0 33.143-2.286 48.571-6.286-84.571-17.143-148-91.429-148-181.143v-2.286c24.571 13.714 53.143 22.286 83.429 23.429-49.714-33.143-82.286-89.714-82.286-153.714 0-34.286 9.143-65.714 25.143-93.143 90.857 112 227.429 185.143 380.571 193.143-2.857-13.714-4.571-28-4.571-42.286 0-101.714 82.286-184.571 184.571-184.571 53.143 0 101.143 22.286 134.857 58.286 41.714-8 81.714-23.429 117.143-44.571-13.714 42.857-42.857 78.857-81.143 101.714 37.143-4 73.143-14.286 106.286-28.571z"></path>
+            </svg>
+            <svg viewBox="0 0 877.7142857142857 1024" className="home-icon06">
+              <path d="M585.143 512c0-80.571-65.714-146.286-146.286-146.286s-146.286 65.714-146.286 146.286 65.714 146.286 146.286 146.286 146.286-65.714 146.286-146.286zM664 512c0 124.571-100.571 225.143-225.143 225.143s-225.143-100.571-225.143-225.143 100.571-225.143 225.143-225.143 225.143 100.571 225.143 225.143zM725.714 277.714c0 29.143-23.429 52.571-52.571 52.571s-52.571-23.429-52.571-52.571 23.429-52.571 52.571-52.571 52.571 23.429 52.571 52.571zM438.857 152c-64 0-201.143-5.143-258.857 17.714-20 8-34.857 17.714-50.286 33.143s-25.143 30.286-33.143 50.286c-22.857 57.714-17.714 194.857-17.714 258.857s-5.143 201.143 17.714 258.857c8 20 17.714 34.857 33.143 50.286s30.286 25.143 50.286 33.143c57.714 22.857 194.857 17.714 258.857 17.714s201.143 5.143 258.857-17.714c20-8 34.857-17.714 50.286-33.143s25.143-30.286 33.143-50.286c22.857-57.714 17.714-194.857 17.714-258.857s5.143-201.143-17.714-258.857c-8-20-17.714-34.857-33.143-50.286s-30.286-25.143-50.286-33.143c-57.714-22.857-194.857-17.714-258.857-17.714zM877.714 512c0 60.571 0.571 120.571-2.857 181.143-3.429 70.286-19.429 132.571-70.857 184s-113.714 67.429-184 70.857c-60.571 3.429-120.571 2.857-181.143 2.857s-120.571 0.571-181.143-2.857c-70.286-3.429-132.571-19.429-184-70.857s-67.429-113.714-70.857-184c-3.429-60.571-2.857-120.571-2.857-181.143s-0.571-120.571 2.857-181.143c3.429-70.286 19.429-132.571 70.857-184s113.714-67.429 184-70.857c60.571-3.429 120.571-2.857 181.143-2.857s120.571-0.571 181.143 2.857c70.286 3.429 132.571 19.429 184 70.857s67.429 113.714 70.857 184c3.429 60.571 2.857 120.571 2.857 181.143z"></path>
+            </svg>
+            <svg viewBox="0 0 602.2582857142856 1024" className="home-icon08">
+              <path d="M548 6.857v150.857h-89.714c-70.286 0-83.429 33.714-83.429 82.286v108h167.429l-22.286 169.143h-145.143v433.714h-174.857v-433.714h-145.714v-169.143h145.714v-124.571c0-144.571 88.571-223.429 217.714-223.429 61.714 0 114.857 4.571 130.286 6.857z"></path>
+            </svg>
+          </div>
+        </div>
+      </header>
+      <div className="home-hero">
+        <span className="home-text10">
+          <span className="home-text11">Lorem ipsum dolor sit amet</span>
+        </span>
+        <h1 className="home-text12">Magnificent things are very simple</h1>
+        <button className="home-button button">Order Now</button>
+      </div>
+      <div className="home-features">
+        <h1 className="home-text13">
+          <span>Discover Mas&apos;Tul</span>
+          <br></br>
+        </h1>
+        <div className="home-separator"></div>
+        <div className="home-container03">
+          <div className="home-container04">
+            <FeatureCard4
+              title="The Resort"
+              rootClassName="rootClassName1"
+            ></FeatureCard4>
+            <FeatureCard4
+              title="The Club"
+              rootClassName="rootClassName4"
+            ></FeatureCard4>
+            <FeatureCard4
+              title="The Restaurant"
+              rootClassName="rootClassName3"
+            ></FeatureCard4>
+            <FeatureCard4
+              title="Top Staff"
+              rootClassName="rootClassName2"
+            ></FeatureCard4>
+          </div>
+          <img
+            alt="image"
+            src="https://images.unsplash.com/photo-1496318447583-f524534e9ce1?ixid=Mnw5MTMyMXwwfDF8c2VhcmNofDIwfHx0cm9waWMlMjBkcmlua3xlbnwwfHx8fDE2NzU0MzQ3OTg&amp;ixlib=rb-4.0.3&amp;w=500"
+            className="home-image1"
+          />
+        </div>
+      </div>
+      <div className="home-gallery">
+        <div className="home-container05">
+          <div className="home-container06">
+            <div className="home-container07">
+              <GalleryCard2
+                image_src="https://images.unsplash.com/photo-1627020730793-2ccb5cd55e99?ixid=Mnw5MTMyMXwwfDF8c2VhcmNofDE5fHxjbHVifGVufDB8fHx8MTY3NTQzNDcyNw&amp;ixlib=rb-4.0.3&amp;h=1500"
+                rootClassName="rootClassName2"
+              ></GalleryCard2>
+            </div>
+            <div className="home-container08">
+              <GalleryCard2
+                image_src="https://images.unsplash.com/photo-1601297543584-575ace20ed5b?ixid=Mnw5MTMyMXwwfDF8c2VhcmNofDI2fHxjbHVifGVufDB8fHx8MTY3NTQzNDc0NA&amp;ixlib=rb-4.0.3&amp;h=1500"
+                rootClassName="rootClassName3"
+              ></GalleryCard2>
+            </div>
+          </div>
+          <div className="home-container09">
+            <GalleryCard2
+              image_src="https://images.unsplash.com/photo-1597075687490-8f673c6c17f6?ixid=Mnw5MTMyMXwwfDF8c2VhcmNofDE3fHxiYXJ8ZW58MHx8fHwxNjc1NDM0NzU3&amp;ixlib=rb-4.0.3&amp;h=1500"
+              rootClassName="rootClassName"
+            ></GalleryCard2>
+          </div>
+        </div>
+        <div className="home-container10">
+          <GalleryCard2
+            image_src="https://images.unsplash.com/photo-1525268323446-0505b6fe7778?ixid=Mnw5MTMyMXwwfDF8c2VhcmNofDF8fGJhcnxlbnwwfHx8fDE2NzU0MzQ3NTc&amp;ixlib=rb-4.0.3&amp;h=1500"
+            rootClassName="rootClassName4"
+          ></GalleryCard2>
+        </div>
+      </div>
+      <div className="home-blog">
+        <BlogPostCard4
+          label="Travel"
+          image_src="https://images.unsplash.com/photo-1638061758086-7f577cf7ab25?ixid=Mnw5MTMyMXwwfDF8c2VhcmNofDEwfHx0cm9waWMlMjBkcmlua3xlbnwwfHx8fDE2NzU0MzQ3OTg&amp;ixlib=rb-4.0.3&amp;w=1500"
+          rootClassName="rootClassName1"
+        ></BlogPostCard4>
+        <div className="home-container11">
+          <BlogPostCard4
+            label="Club Party"
+            image_src="https://images.unsplash.com/photo-1527271982979-83fea3eb3582?ixid=Mnw5MTMyMXwwfDF8c2VhcmNofDM3fHx0cm9waWMlMjBkcmlua3xlbnwwfHx8fDE2NzU0MzQ4NTM&amp;ixlib=rb-4.0.3&amp;w=1500"
+            rootClassName="rootClassName2"
+          ></BlogPostCard4>
+          <BlogPostCard4
+            image_src="https://images.unsplash.com/photo-1499638673689-79a0b5115d87?ixid=Mnw5MTMyMXwwfDF8c2VhcmNofDI0fHx0cm9waWMlMjBkcmlua3xlbnwwfHx8fDE2NzU0MzQ3OTg&amp;ixlib=rb-4.0.3&amp;w=1500"
+            rootClassName="rootClassName3"
+          ></BlogPostCard4>
+        </div>
+        <BlogPostCard4
+          date="JULY 10"
+          image_src="https://images.unsplash.com/photo-1638061714820-ab12cbbeb87e?ixid=Mnw5MTMyMXwwfDF8c2VhcmNofDEyfHx0cm9waWMlMjBkcmlua3xlbnwwfHx8fDE2NzU0MzQ3OTg&amp;ixlib=rb-4.0.3&amp;w=1500"
+          rootClassName="rootClassName4"
+        ></BlogPostCard4>
+      </div>
+      <div className="home-testimonial">
+        <div className="home-container12">
+          <div className="home-container13">
+            <div className="home-container14">
+              <TestimonialCard2
+                profile_src="https://images.unsplash.com/photo-1638061758086-7f577cf7ab25?ixid=Mnw5MTMyMXwwfDF8c2VhcmNofDEwfHx0cm9waWMlMjBkcmlua3xlbnwwfHx8fDE2NzU0MzQ3OTg&amp;ixlib=rb-4.0.3&amp;h=200"
+                rootClassName="rootClassName1"
+              ></TestimonialCard2>
+            </div>
+            <TestimonialCard2
+              profile_src="https://images.unsplash.com/photo-1638061714820-ab12cbbeb87e?ixid=Mnw5MTMyMXwwfDF8c2VhcmNofDEyfHx0cm9waWMlMjBkcmlua3xlbnwwfHx8fDE2NzU0MzQ3OTg&amp;ixlib=rb-4.0.3&amp;h=200"
+              rootClassName="rootClassName2"
+            ></TestimonialCard2>
+          </div>
+          <div className="home-container15">
+            <TestimonialCard4
+              picture_src="https://images.unsplash.com/photo-1482555670981-4de159d8553b?ixid=Mnw5MTMyMXwwfDF8c2VhcmNofDEzfHx0cm9waWMlMjBwYXJ0eSUyMGdpcmx8ZW58MHx8fHwxNjc1NDM0OTQz&amp;ixlib=rb-4.0.3&amp;h=200"
+              profile_src="https://images.unsplash.com/photo-1553184118-d20774c4c1db?ixid=Mnw5MTMyMXwwfDF8c2VhcmNofDI0fHxwb3J0cmFpdCUyMGJ3fGVufDB8fHx8MTYyNjQ1MjI0MA&amp;ixlib=rb-1.2.1&amp;h=1200"
+              rootClassName="rootClassName"
+            ></TestimonialCard4>
+          </div>
+        </div>
+      </div>
+      <footer className="home-footer">
+        <div className="home-container16">
+          <div className="home-logo1">
+            <img
+              alt="logo"
+              src="/playground_assets/mastulogo-1500h.webp"
+              className="home-image2"
+            />
+            <span className="home-text16">Resort | Restaurant | Club </span>
+          </div>
+          <div className="home-links-container">
+            <div className="home-container17">
+              <div className="home-product-container">
+                <span className="home-text17">Product</span>
+                <span className="home-text18">Features</span>
+                <span className="home-text19">Pricing</span>
+                <span className="home-text20">Tutorials</span>
+                <span className="home-text21">Releases</span>
+              </div>
+              <div className="home-company-container">
+                <span className="home-text22">Company</span>
+                <span className="home-text23">About</span>
+                <span className="home-text24">Careers</span>
+                <span className="home-text25">Contact</span>
+                <span className="home-text26">Blog</span>
+              </div>
+            </div>
+            <div className="home-container18">
+              <div className="home-contact">
+                <span className="home-text27">Contact Us</span>
+                <span className="home-text28">hello@maya.resort</span>
+                <span className="home-text29">+123 (4567) 890</span>
+              </div>
+              <div className="home-socials">
+                <span className="home-text30">Follow Us</span>
+                <div className="home-icon-group1">
+                  <svg
+                    viewBox="0 0 950.8571428571428 1024"
+                    className="home-icon10"
+                  >
+                    <path d="M925.714 233.143c-25.143 36.571-56.571 69.143-92.571 95.429 0.571 8 0.571 16 0.571 24 0 244-185.714 525.143-525.143 525.143-104.571 0-201.714-30.286-283.429-82.857 14.857 1.714 29.143 2.286 44.571 2.286 86.286 0 165.714-29.143 229.143-78.857-81.143-1.714-149.143-54.857-172.571-128 11.429 1.714 22.857 2.857 34.857 2.857 16.571 0 33.143-2.286 48.571-6.286-84.571-17.143-148-91.429-148-181.143v-2.286c24.571 13.714 53.143 22.286 83.429 23.429-49.714-33.143-82.286-89.714-82.286-153.714 0-34.286 9.143-65.714 25.143-93.143 90.857 112 227.429 185.143 380.571 193.143-2.857-13.714-4.571-28-4.571-42.286 0-101.714 82.286-184.571 184.571-184.571 53.143 0 101.143 22.286 134.857 58.286 41.714-8 81.714-23.429 117.143-44.571-13.714 42.857-42.857 78.857-81.143 101.714 37.143-4 73.143-14.286 106.286-28.571z"></path>
+                  </svg>
+                  <svg
+                    viewBox="0 0 877.7142857142857 1024"
+                    className="home-icon12"
+                  >
+                    <path d="M585.143 512c0-80.571-65.714-146.286-146.286-146.286s-146.286 65.714-146.286 146.286 65.714 146.286 146.286 146.286 146.286-65.714 146.286-146.286zM664 512c0 124.571-100.571 225.143-225.143 225.143s-225.143-100.571-225.143-225.143 100.571-225.143 225.143-225.143 225.143 100.571 225.143 225.143zM725.714 277.714c0 29.143-23.429 52.571-52.571 52.571s-52.571-23.429-52.571-52.571 23.429-52.571 52.571-52.571 52.571 23.429 52.571 52.571zM438.857 152c-64 0-201.143-5.143-258.857 17.714-20 8-34.857 17.714-50.286 33.143s-25.143 30.286-33.143 50.286c-22.857 57.714-17.714 194.857-17.714 258.857s-5.143 201.143 17.714 258.857c8 20 17.714 34.857 33.143 50.286s30.286 25.143 50.286 33.143c57.714 22.857 194.857 17.714 258.857 17.714s201.143 5.143 258.857-17.714c20-8 34.857-17.714 50.286-33.143s25.143-30.286 33.143-50.286c22.857-57.714 17.714-194.857 17.714-258.857s5.143-201.143-17.714-258.857c-8-20-17.714-34.857-33.143-50.286s-30.286-25.143-50.286-33.143c-57.714-22.857-194.857-17.714-258.857-17.714zM877.714 512c0 60.571 0.571 120.571-2.857 181.143-3.429 70.286-19.429 132.571-70.857 184s-113.714 67.429-184 70.857c-60.571 3.429-120.571 2.857-181.143 2.857s-120.571 0.571-181.143-2.857c-70.286-3.429-132.571-19.429-184-70.857s-67.429-113.714-70.857-184c-3.429-60.571-2.857-120.571-2.857-181.143s-0.571-120.571 2.857-181.143c3.429-70.286 19.429-132.571 70.857-184s113.714-67.429 184-70.857c60.571-3.429 120.571-2.857 181.143-2.857s120.571-0.571 181.143 2.857c70.286 3.429 132.571 19.429 184 70.857s67.429 113.714 70.857 184c3.429 60.571 2.857 120.571 2.857 181.143z"></path>
+                  </svg>
+                  <svg
+                    viewBox="0 0 602.2582857142856 1024"
+                    className="home-icon14"
+                  >
+                    <path d="M548 6.857v150.857h-89.714c-70.286 0-83.429 33.714-83.429 82.286v108h167.429l-22.286 169.143h-145.143v433.714h-174.857v-433.714h-145.714v-169.143h145.714v-124.571c0-144.571 88.571-223.429 217.714-223.429 61.714 0 114.857 4.571 130.286 6.857z"></path>
+                  </svg>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="home-separator1"></div>
+        <span className="home-text31">
+          © 2021 Mas&apos;Tul, All Rights Reserved.
+        </span>
+      </footer>
+    </div>
+  )
+}
+
+export default Home
---
+++ tailwind.config.js
@@ -0,0 +1,62 @@
+module.exports = {
+  "theme": {
+    "extend": {
+      "colors": {
+        "Danger": {
+          "300": "#A22020",
+          "500": "#BF2626",
+          "700": "#E14747"
+        },
+        "Primary": {
+          "100": "#003EB3",
+          "300": "#0074F0",
+          "500": "#14A9FF",
+          "700": "#85DCFF"
+        },
+        "Gray": {
+          "500": "#595959",
+          "700": "#999999",
+          "900": "#D9D9D9",
+          "White": "#FFFFFF",
+          "Black": "#000000"
+        },
+        "Success": {
+          "300": "#199033",
+          "500": "#32A94C",
+          "700": "#4CC366"
+        },
+        "foreground": "#000000",
+        "background": "#FFFFFF"
+      },
+      "spacing": {
+        "TwoUnits": "32px",
+        "OneAndHalfUnits": "24px",
+        "ThreeUnits": "48px",
+        "SixUnits": "96px",
+        "FiveUnits": "80px",
+        "Unit": "16px",
+        "FourUnits": "64px",
+        "HalfUnit": "8px"
+      },
+      "borderRadius": {
+        "Round": "50%",
+        "Radius4": "4px",
+        "Radius8": "8px",
+        "Radius2": "2px"
+      },
+      "scale": {
+        "Small": "48px",
+        "MaxWidth": "1400px",
+        "XLarge": "192px",
+        "Medium": "96px",
+        "XSmall": "16px",
+        "Large": "144px",
+        "XXLarge": "288px"
+      }
+    }
+  },
+  "plugins": [],
+  "content": [
+    "./src/**/*.{js,ts,jsx,tsx}"
+  ]
+}
